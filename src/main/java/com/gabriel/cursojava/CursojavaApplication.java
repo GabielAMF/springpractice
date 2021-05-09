cc59673de9d3fde83bddfcc5d7b80797aa681f2a
Gabriel Ferreira <dieofmoon@hotmail.com>
2021-05-09 16:38:47 -0300

    produtos e associacoes

--- src/main/java/com/gabriel/cursojava/CursojavaApplication.java
+++ src/main/java/com/gabriel/cursojava/CursojavaApplication.java
@@ -1,5 +1,6 @@
 package com.gabriel.cursojava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabriel.cursojava.domain.Categoria;
+import com.gabriel.cursojava.domain.Produto;
 import com.gabriel.cursojava.repositories.CategoriaRepository;
+import com.gabriel.cursojava.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursojavaApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository catRepo;
+	
+	@Autowired
+	private ProdutoRepository prodRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojavaApplication.class, args);
@@ -24,11 +30,19 @@ public class CursojavaApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora", 800.00);
+		Produto p3 = new Produto(null,"Mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().add(p2);
 		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().add(cat1);
 		
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
-		
+		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
---
+++ src/main/java/com/gabriel/cursojava/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.gabriel.cursojava.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabriel.cursojava.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
