09c2ac85eb38f60e3411096a5d3b88d76fa4e78f
Gabriel Ferreira <dieofmoon@hotmail.com>
2021-05-09 18:17:39 -0300

    Estado e Cidade

--- src/main/java/com/gabriel/cursojava/CursojavaApplication.java
+++ src/main/java/com/gabriel/cursojava/CursojavaApplication.java
@@ -1,6 +1,5 @@
 package com.gabriel.cursojava;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabriel.cursojava.domain.Categoria;
+import com.gabriel.cursojava.domain.Cidade;
+import com.gabriel.cursojava.domain.Estado;
 import com.gabriel.cursojava.domain.Produto;
 import com.gabriel.cursojava.repositories.CategoriaRepository;
+import com.gabriel.cursojava.repositories.CidadeRepository;
+import com.gabriel.cursojava.repositories.EstadoRepository;
 import com.gabriel.cursojava.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +24,12 @@ public class CursojavaApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository prodRepo;
 	
+	@Autowired
+	private EstadoRepository estadoRepo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojavaApplication.class, args);
 	}
@@ -30,9 +39,18 @@ public class CursojavaApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		
 		Produto p1 = new Produto(null,"Computador", 2000.00);
 		Produto p2 = new Produto(null,"Impressora", 800.00);
 		Produto p3 = new Produto(null,"Mouse", 80.00);
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().add(p2);
@@ -41,8 +59,13 @@ public class CursojavaApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().add(cat1);
 		
+		est1.getCidades().add(c1);
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
 		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepo.saveAll(Arrays.asList(est1,est2));
+		cidadeRepo.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
